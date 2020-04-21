b78253ec595029a6fdb7bceb5adf4a2684bc28d8
Jay <jaychenblue@gmail.com>
2020-04-21 16:05:20 -0400

    [#12] Used props to display custom game cards
    Figured out how to display game info based on the props; removed Bootstrap from the project; deleted TestButton.js because it's no longer needed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import TestButton from './TestButton.js';
 import Game from './Game.js';
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Game />
+      <Game title="Grand Theft Auto V"
+          platform="Steam"
+          image="gta5.jpg"
+          status="backlog"
+          description="Enter the lives of criminals as they risk everything in a series of dangerous heists."
+      />
+      <Game title="Red Dead Redemption 2"
+          platform="PS4"
+          image="rdr2.jpg"
+          description="Rob, steal and fight your way across the rugged heartland of America."
+      />
+      <Game title="Pokémon: Let's Go, Eevee!"
+          platform="Switch"
+          image="lg_eevee.jpg"
+          description="Take a Pokémon journey to the Kanto region with your energetic partner, Eevee!"
+      />
     </>
   );
 }
