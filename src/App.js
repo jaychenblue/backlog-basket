0cf90c1a9153fe173703f7e94031ebd53622a1dd
Jay <jaychenblue@gmail.com>
2020-04-21 18:51:35 -0400

    [#15] Added a horizontal divider and more games
    The horizontal divider may be removed later as I try to figure out a way to dynamically display the games by status

--- src/App.js
+++ src/App.js
@@ -5,22 +5,67 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Game title="Grand Theft Auto V"
-          platform="Steam"
-          image="gta5.jpg"
-          status="backlog"
-          description="Enter the lives of criminals as they risk everything in a series of dangerous heists."
-      />
-      <Game title="Red Dead Redemption 2"
-          platform="PS4"
-          image="rdr2.jpg"
-          description="Rob, steal and fight your way across the rugged heartland of America."
-      />
-      <Game title="Pokémon: Let's Go, Eevee!"
-          platform="Switch"
-          image="lg_eevee.jpg"
-          description="Take a Pokémon journey to the Kanto region with your energetic partner, Eevee!"
-      />
+      <div class="ui grid">
+        <div class="row center aligned">
+          <div class="three wide column">
+            <Game title="Grand Theft Auto V"
+              platform="Steam"
+              image="gta5.jpg"
+              status="backlog"
+              description="Enter the lives of criminals as they risk everything in a series of dangerous heists."
+            />
+          </div>
+          <div class="three wide column">
+            <Game title="Red Dead Redemption 2"
+              platform="PS4"
+              image="rdr2.jpg"
+              description="Rob, steal and fight your way across the rugged heartland of America."
+            />
+          </div>
+          <div class="three wide column">
+            <Game title="Pokémon: Let's Go, Eevee!"
+              platform="Switch"
+              image="eevee.jpg"
+              description="Take a Pokémon journey to the Kanto region with your energetic partner, Eevee!"
+            />
+          </div>
+          <div class="three wide column">
+            <Game title="Assassin's Creed Odyssey"
+              platform="Steam"
+              image="odyssey.jpg"
+              description="Write your own epic odyssey and become a legendary Spartan hero."
+            />
+          </div>
+          <div class="three wide column">
+            <Game title="Sleeping Dogs"
+              platform="PS4"
+              image="sleeping_dogs.jpg"
+              description="Play as an undercover cop trying to take down the Triads from the inside out."
+            />
+          </div>
+        </div>
+      </div>
+
+      <br />
+      <h3 class="ui horizontal divider header">
+        <i class="gamepad icon"></i>
+        <div class="content">
+          My Library
+        </div>
+      </h3>
+      <br />
+
+      <div class="ui grid">
+        <div class="row center aligned">
+          <div class="three wide column">
+            <Game title="Mario Kart 8"
+              platform="Wii U"
+              image="mk8.png"
+              description="Hit the road as you race and battle against your friends!"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
